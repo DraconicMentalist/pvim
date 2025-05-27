06ca3a5c54948be4db0fa98c74c57b7bd6d10a1f
Camilla <DraconicMentalist@gmail.com>
2025-05-27 19:01:33 -0400

    added treesitter and mason

--- config/init.lua
+++ config/init.lua
@@ -3,5 +3,9 @@ local Plug = vim.fn['plug#']
 
 vim.call('plug#begin', (os.getenv("PVIM") .. "config/plugged/"))
 Plug('tpope/vim-sensible')
+Plug("nvim-treesitter/nvim-treesitter", {["do"] = ":TSUpdate"})
+Plug("mason-org/mason.nvim")
 vim.call('plug#end')
-print()
+
+require'nvim-treesitter.configs'.setup{highlight={enable=true}}
+require("mason").setup()
